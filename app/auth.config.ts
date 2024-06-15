57d50419abbd72fdbcc8bbc3d54386cd8a7a143d
sayav <42376060+sayav@users.noreply.github.com>
2024-06-15 17:43:49 +0100

    prevent redirect if logged in

--- app/auth.config.ts
+++ app/auth.config.ts
@@ -22,8 +22,6 @@ export const authConfig = {
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL("/protected", nextUrl));
       }
 
       return true;
