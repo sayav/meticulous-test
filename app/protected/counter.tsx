6bae6ddce3d5880e9e3b226da7341b5f04026548
sayav <42376060+sayav@users.noreply.github.com>
2024-06-15 17:07:27 +0100

    add counter

---
+++ app/protected/counter.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useState } from "react";
+import { increment } from "../actions";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = async () => {
+    const newCount = await increment(count);
+    setCount(newCount);
+  };
+
+  return (
+    <button style={{ width: 40, background: "green" }} onClick={handleClick}>
+      {count}
+    </button>
+  );
+}
