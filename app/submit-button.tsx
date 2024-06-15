36fbb056c3d6aac8055374b8d445e51aeac38e6c
sayav <42376060+sayav@users.noreply.github.com>
2024-06-15 17:27:46 +0100

    add bugs to unprotected routes

--- app/submit-button.tsx
+++ app/submit-button.tsx
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 
-import { useFormStatus } from 'react-dom';
+import { useFormStatus } from "react-dom";
 
 export function SubmitButton({ children }: { children: React.ReactNode }) {
   const { pending } = useFormStatus();
 
   return (
     <button
-      type={pending ? 'button' : 'submit'}
+      type={pending ? "button" : "submit"}
       aria-disabled={pending}
-      className="flex h-10 w-full items-center justify-center rounded-md border text-sm transition-all focus:outline-none"
+      className="flex h-10 w-full items-center justify-center rounded-md border text-sm transition-all focus:outline-none bg-red-200"
     >
       {children}
       {pending && (
@@ -35,7 +35,7 @@ export function SubmitButton({ children }: { children: React.ReactNode }) {
         </svg>
       )}
       <span aria-live="polite" className="sr-only" role="status">
-        {pending ? 'Loading' : 'Submit form'}
+        {pending ? "Loading" : "Submit form"}
       </span>
     </button>
   );
